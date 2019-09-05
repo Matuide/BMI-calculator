150e46028cb2f16c64e71c4fbd5bdc7604b553bb
Matthew Coleman <K31073@eltham-college.org.uk>
2019-09-05 12:52:30 +0100

    final

--- BMI calculator/Program.cs
+++ BMI calculator/Program.cs
@@ -6,8 +6,24 @@ namespace BMI_calculator
     {
         static void Main(string[] args)
         {
+            string weight;
+            string height;
+            double mass;
 
-            Console.WriteLine("Hello World!");
+            double height_2;
+            double weight_2;
+            double bmi;
+            Console.WriteLine("Enter your weight in Kilograms");
+            weight = Console.ReadLine();
+            Console.WriteLine("enter your height in metres");
+            height = Console.ReadLine();
+            weight_2 = Convert.ToDouble(weight);
+            height_2 = Convert.ToDouble(height);
+
+            mass = height_2 * height_2;
+            bmi = weight_2 / mass  ;
+            Console.WriteLine("your bmi is {0:00.00}" , bmi);
+
         }
     }
 }
