27cad1c7401db79a5c5da1166e97e7538e7364e7
Matthew Coleman <K31073@eltham-college.org.uk>
2019-09-05 12:12:48 +0100

    Add project files.

---
+++ BMI calculator/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BMI_calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
